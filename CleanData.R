bb6d4b2dc142335d22a0d3e910516a5297a2e586
skyprince999 <aakashg80@gmail.com>
2015-02-25 22:55:58 +0530

    Quarterly data

--- CleanData.R
+++ CleanData.R
@@ -1,9 +1,9 @@
-setwd("D:/NPA in Banking Sector")
+
 require(xlsx)
 output<-data.frame()
 sheets<-list()
-for(i in 1:14){
-  sheets[[i]]<-read.xlsx("Private Banks_1.xlsx",i)
+for(i in 1:38){
+  sheets[[i]]<-read.xlsx("Private_Banks_Quarterly.xlsx",i)
   bankName<-rep(names(sheets[[i]])[1],18)
   privatePublic<-rep("Private",18)
   quarters<-t(rev(sheets[[i]][6,(2:19)]))
@@ -13,19 +13,8 @@ for(i in 1:14){
   
 }
 
-for(i in 1:13){
-  sheets[[i]]<-read.xlsx("Public Banks_1.xlsx",i)
-  bankName<-rep(names(sheets[[i]])[1],18)
-  privatePublic<-rep("Public",18)
-  #quarters<-t(rev(sheets[[i]][6,(2:19)]))
-  bank_data<-t(rev(sheets[[i]][c(26,43:45,47),(2:19)]))
-  #Append all data
-  output<-rbind(output,cbind(bankName,privatePublic,quarters,bank_data))
-  
-}
-
-for(i in 1:11){
-  sheets[[i]]<-read.xlsx("Public Banks_2.xlsx",i)
+for(i in 1:24){
+  sheets[[i]]<-read.xlsx("Public Banks_Quarterly.xlsx",i)
   bankName<-rep(names(sheets[[i]])[1],18)
   privatePublic<-rep("Public",18)
   #quarters<-t(rev(sheets[[i]][6,(2:19)]))
--- VizData.R
+++ VizData.R
@@ -3,15 +3,18 @@ require(xlsx)
 
 #Read datafile
 #setwd("D:/NPA in Banking Sector")
-#bank_data<-read.xlsx("datafile.xlsx",1,header=TRUE,colClasses=c("character","character","Date","double","double","double","double","double"))
+bank_data<-read.xlsx("datafile.xlsx",1,header=TRUE,colClasses=c("character","character","Date","double","double","double","double","double"))
 
 #Motionchart the data
 M=gvisMotionChart(data=bank_data,idvar="Bank.Name",timevar="Quarter",
                   xvar="Net.NPA",yvar="RoA",colorvar="Private.Public",
                   sizevar="Net.Profit",
                   options=list(vAxes="[{viewWindowMode:'explicit',
-  		viewWindow:{min:0, max:10}}]",title="NPA Visualization of Indian Banks"),
+  		viewWindow:{min:-0.01, max:0.025}}]",title="NPA Visualization of Indian Banks"),
                   chartid="NPAViz",)
 plot(M)
 
 cat(unlist(M$html),file="NPAViz.html")
+
+#For creating Google gadgets
+#cat(createGoogleGadget(M), file="myGadget.xml")
